c5d76b1fff2174a9b4abe64c22e5adad937365a2
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-15 17:17:56 +0100

    Added function to change play/pause button on click

--- scripts.js
+++ scripts.js
@@ -12,12 +12,20 @@ const ranges = player.querySelectorAll('.player__slider');
 // Build functions
 
 function togglePlay() {
-    if(video.paused) {
-        video.play()
+    const method = video.paused ? 'play' : 'pause';
+    video[method]()
     }
-    else {video.pause()
-    }
+
+
+function updateButton() {
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
 }
 
 
 // Add event listeners
+
+video.addEventListener('click' , togglePlay)
+video.addEventListener('play' , updateButton)
+video.addEventListener('pause' , updateButton)
+toggle.addEventListener('click' , togglePlay)
