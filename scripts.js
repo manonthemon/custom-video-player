6931efc09915297e4c0409b7b0566c53928445e9
Manonthemon <klimowicz.maciej@gmail.com>
2023-05-15 17:29:56 +0100

    Added progress bar updating

--- scripts.js
+++ scripts.js
@@ -1,36 +1,53 @@
-// Get Elements 
+// Get Elements
 
-const player = document.querySelector('.player');
-const video = player.querySelector('.viewer')
-const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('.toggle');
-const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('.player__slider');
-
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
 
 // Build functions
 
 function togglePlay() {
-    const method = video.paused ? 'play' : 'pause';
-    video[method]()
-    }
-
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
 
 function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
-    toggle.textContent = icon;
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
 }
 
 function skip() {
-    console.log(this.dataset.skip);
-    video.currentTime += parseFloat(this.dataset.skip)
+  console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+  console.log(this.name);
+  console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) *100;
+    progressBar.style.flexBasis = `${percent}%`;
 }
 
 // Add event listeners
 
-video.addEventListener('click' , togglePlay)
-video.addEventListener('play' , updateButton)
-video.addEventListener('pause' , updateButton)
-toggle.addEventListener('click' , togglePlay)
-skipButtons.forEach(button => button.addEventListener('click', skip))
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((ranges) =>
+  ranges.addEventListener("change", handleRangeUpdate)
+);
+ranges.forEach((ranges) =>
+  ranges.addEventListener("mousemove", handleRangeUpdate)
+);
+
